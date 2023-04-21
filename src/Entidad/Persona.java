ab75e02a1a72fb8026faac3833e613dfa898e289
Gustavo Rivero <gustirivero@hotmail.com>
2023-04-21 12:49:42 -0300

    iniciando repositorio

---
+++ src/Entidad/Persona.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Persona {
+    public String nombre;
+    public  int dni;
+    public int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int dni, int edad) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", dni=" + dni + ", edad=" + edad + '}';
+    }
+
+    
+
+
+}
+
